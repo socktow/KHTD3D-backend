31139ca392fc4e88f774d706e935e78137f92dba
socktow <mjssdn95@gmail.com>
2024-12-07 00:03:10 +0700

    cdn

--- Router/Admin/Upload.js
+++ Router/Admin/Upload.js
@@ -2,43 +2,46 @@ const express = require("express");
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
-
+const SftpClient = require("ssh2-sftp-client");
 const router = express.Router();
-const PORT = 4000;
+const sftp = new SftpClient();
+const SFTP_HOST = process.env.SFTP_HOST;
+const SFTP_USER = process.env.SFTP_USER;
+const SFTP_PASSWORD = process.env.SFTP_PASSWORD;
+const SFTP_PORT = process.env.SFTP_PORT;
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+const generateFileName = (originalName) => {
+  const timestamp = Date.now();
+  const extension = path.extname(originalName);
+  return `image_${timestamp}${extension}`;
+};
+router.post("/upload", upload.single("image"), async (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ success: false, message: "No file uploaded" });
+  }
+  try {
+    await sftp.connect({
+      host: SFTP_HOST,
+      username: SFTP_USER,
+      password: SFTP_PASSWORD,
+      port: SFTP_PORT,
+    });
+    const newFileName = generateFileName(req.file.originalname);
+    const remoteFilePath = `/home/gmbbesh/public_html/cdn.kiemhieptinhduyen.com/images/${newFileName}`;
+    await sftp.put(req.file.buffer, remoteFilePath);
 
-// // Tạo thư mục nếu chưa tồn tại
-// const uploadDir = "./upload/images";
-// if (!fs.existsSync(uploadDir)) {
-//   fs.mkdirSync(uploadDir, { recursive: true });
-// }
-
-// // Cấu hình lưu trữ của multer
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, uploadDir);
-//   },
-//   filename: (req, file, cb) => {
-//     cb(
-//       null,
-//       `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
-//     );
-//   },
-// });
-
-// const upload = multer({ storage: storage });
-
-// // Endpoint upload ảnh
-// router.post("/upload", upload.single("image"), (req, res) => {
-//   if (!req.file) {
-//     return res
-//       .status(400)
-//       .json({ success: false, message: "No file uploaded" });
-//   }
-//   res.json({
-//     success: true,
-//     image_url: `http://localhost:${PORT}/images/${req.file.filename}`,
-//     message: "File uploaded successfully",
-//   });
-// });
+    res.json({
+      success: true,
+      image_url: `https://cdn.kiemhieptinhduyen.com/images/${newFileName}`,
+      message: "File uploaded successfully",
+    });
+  } catch (error) {
+    console.error("Error uploading file via SFTP:", error);
+    res.status(500).json({ success: false, message: "Internal server error", error: error.message });
+  } finally {
+    sftp.end();
+  }
+});
 
 module.exports = router;
