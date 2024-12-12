12b259ce1fd3590dfa3b55cb8bf0f283da0731b6
socktow <mjssdn95@gmail.com>
2024-12-13 02:59:06 +0700

    giftcode

--- Router/Admin/GiftCode.js
+++ Router/Admin/GiftCode.js
@@ -3,11 +3,18 @@ const Giftcode = require("../../Schema/GiftcodeSchema");
 const fetchUser = require("../../Settings/FetchUser");
 const router = express.Router();
 
-router.post("/create-giftcode", fetchUser, async (req, res) => {
+router.get("/giftcode", async (req, res) => {
   try {
-    if (!req.isAdmin) {
-      return res.status(403).json({ message: "Access denied. Admin only." });
-    }
+    const giftcodes = await Giftcode.find();
+    res.json(giftcodes);
+  } catch (error) {
+    console.error("Error fetching giftcodes:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+router.post("/create-giftcode", async (req, res) => {
+  try {
     const { name, code, items, title, usage, content, expiryDate } = req.body;
     const parsedItems = items.map((item) => {
       if (!item.itemId || !item.quantity || item.quantity <= 0) {
@@ -34,6 +41,5 @@ router.post("/create-giftcode", fetchUser, async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 });
-
 
 module.exports = router;
