52b272424fa04d6bf6e2f86d7ae0a6725d9fb70c
socktow <mjssdn95@gmail.com>
2024-12-17 00:43:21 +0700

    Game Services

--- GS/Member.js
+++ GS/Member.js
@@ -1,20 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const { query } = require("../Services/MySQLService");
 
-router.get("/", async (req, res) => {
-  try {
-    const rows = await query("SELECT ID, NAME FROM snapshot");
-    const formattedRows = rows.map((row) => ({
-      id: row.ID,
-      name: row.NAME,
-    }));
 
-    res.json({ charlist: formattedRows });
-  } catch (error) {
-    console.error("MySQL Query Error:", error);
-    res.status(500).json({ error: "Unable to retrieve data" });
-  }
-});
+router.get("/");
 
 module.exports = router;
--- index.js
+++ index.js
@@ -5,36 +5,23 @@ const userRoutes = require("./Routes/userRoutes");
 const gameRoutes = require("./Routes/gameRoutes");
 const applyAdminMiddleware = require("./Routes/adminRoutes");
 const cors = require("cors");
-
 const app = express();
 const PORT = process.env.PORT || 4000;
+require('dotenv').config();
 const MONGO_URI = process.env.MONGO_URI;
-
 setupMiddleware(app);
-
-// Chỉ kết nối MongoDB khi có userRoutes hoặc applyAdminMiddleware
-if (userRoutes.length > 0 || applyAdminMiddleware) {
-  console.log('Connecting to MongoDB...');
-  connectToMongoDB(MONGO_URI);
-}
-
-// Client Services (User & Admin Routes)
+connectToMongoDB(MONGO_URI);
 userRoutes.forEach(({ path, router }) => {
   app.use(path, router);
 });
-applyAdminMiddleware(app);
-
-// Game Services (Game Routes sử dụng MySQL, không cần MongoDB)
 gameRoutes.forEach(({ path, router }) => {
   app.use(path, router);
 });
-
-// CORS configuration
+applyAdminMiddleware(app);
 const allowedOrigins = [
   "http://localhost:3000",
   "https://kiemhieptinhduyen.com"
 ];
-
 app.use(cors({
   origin: function (origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
@@ -47,7 +34,6 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "token"]
 }));
 
-// Start the server
 app.listen(PORT, (error) => {
   if (error) {
     console.error("Error starting server:", error);
